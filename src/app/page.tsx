12210baf17556d79ab4f8baf28fbbb599f7b3db3
Christian Schuller <christianschuller.biz@gmail.com>
2024-08-17 11:19:40 +0200

    chore: update project to match vieste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,16 +11,15 @@ export default function HomePage() {
       <section className="py-16 md:py-24 lg:py-32">
         <div className="container flex max-w-screen-md flex-col items-center gap-4 text-center">
           <h1 className="!leading-none font-bold text-3xl sm:text-5xl md:text-6xl lg:text-7xl landing-header py-2">
-            Share <strong>Expenses</strong> <br /> with <strong>Friends</strong>{' '}
-            & <strong>Family</strong>
+            Teile <strong>Ausgaben</strong> <br /> mit <strong>Freunden</strong>
           </h1>
           <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
-            Welcome to your new <strong>Spliit</strong> instance! <br />
-            Customize this page by editing <em>src/app/page.tsx</em>.
+            Hier könnt ihr eure Augaben in <strong>Vieste</strong> teilen!{' '}
+            <br />
           </p>
           <div className="flex gap-2">
             <Button asChild>
-              <Link href="/groups">Go to groups</Link>
+              <Link href="/groups">Legt hier los!</Link>
             </Button>
             <Button asChild variant="secondary">
               <Link href="https://github.com/spliit-app/spliit">
